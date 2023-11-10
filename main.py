f6d4f0aab0c46e38da19701a8955ea9f3afb477b
ggeorgieva <ggeorgieva@GGEORGIEVA_PC>
2023-11-10 14:42:27 +0200

    make socket connection and disconnect

--- main.py
+++ main.py
@@ -59,6 +59,35 @@ def room():
     
     return render_template("room.html")
 
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
