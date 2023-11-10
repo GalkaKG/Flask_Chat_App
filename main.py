31252c8f8c045826130ffec02338d9887f851f42
ggeorgieva <ggeorgieva@GGEORGIEVA_PC>
2023-11-10 12:10:33 +0200

    addes home page and style

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "abcdefsgdhfj"
 socketio = SocketIO(app)
 
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
